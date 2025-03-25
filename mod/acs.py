b008cba150c2a6415473a357748317ba6f32f901
alouicho <alexandre.louichon@etu.ec-lyon.fr>
2025-03-25 09:10:50 +0100

    implémentation phéromones
    Plusieurs choses par rapport à hier :
    - Ajout de noeud_depart, noeud_arrivee en argument de la colonie de fourmis (tu peux le changer pcq tu m'avais dit que t'avais une idée précise en tête)
    - création phéromones avec tau0 à l'initialisation
    - Ajout d'un paramètre colonie : qte_pheromones qui correspond à la qté de phéromones que chaque fourmi peut laisser sur son chemin
    - Dans choix_noeud, l'exploration prend maintenant un voisin de manière aléatoire parmi les voisins disponibles
    - on calcule le temps de trajet  de la fourmi en additionnant à chaque itération du while la distance qu'elle traverse jusqu'à son nouveau noeud
    - Si la fourmi à atteint son objectif, on calcule la qté unitaire de phéromones qu'elle déposera sur le chemin qu'elle a parcouru et on update le graphe des phéromones de la colonie  (attention je n'ai pas inclus la dissipation progressive des phéromones)

--- mod/acs.py
+++ mod/acs.py
@@ -2,86 +2,24 @@ import numpy as np
 import random
 import math
 from mod.graph import GlobalGraph, BusGraph
-
-class AntColonySystem:
-    def __init__(self, graph, colony_id, alpha=1.0, beta=2.0, rho=0.1, q0=0.9, tau0=None):
-        """
-         Initialise un sys
-        """
-        self.graph = graph
-        self.colony_id = colony_id
-        self.alpha = alpha
-        self.beta = beta
-        self.rho = rho
-        self.q0 = q0
-        
-        # Initialisation des phéromones pour la colonie de fourmis, avec des phéromones initiales tau0 sur les arêtes qui existent
-        self.pheromones = {}
-        for i in graph.get_nodes().keys():
-            for j in graph.get_nodes().keys():
-                if i != j and graph.exists_edge(i, j):
-                    self.pheromones[(i, j)] = tau0 if tau0 else 0.1
-
-    def select_next_node(self, current_node, unvisited_nodes):
-        """
-            Sélectionne le prochain nœud en utilisant la règle de transition d'état ACS : 
-                - Exploitation : Choisir le nœud avec la plus grande valeur de phéromones et visibilité
-                - Exploration : Choisir un nœud aléatoire en fonction des probabilités calculées
-        """
-        if not unvisited_nodes:
-            return None
-        
-        # Choix aléatoire entre exploitation et exploration (basé sur q0)
-        q = random.random()
-        if q <= self.q0:  # Exploitation
-            max_val = -float('inf')
-            next_node = None
-            for node in unvisited_nodes:
-                if self.graph.exists_edge(current_node, node):
-                    # Calcul de la visibilité (inverse du temps de parcours)
-                    visibility = 1.0 / self.graph.get_travel_time(current_node, node)
-
-                    # Le calcul de cette valeur dépend de la fonction qui a été choisie (cf enoncé)
-                    val = self.pheromones.get((current_node, node), 0) ** self.alpha * visibility ** self.beta
-
-                    # Algo de max pour déterminer le prochain noeud avec le plus de phéromones
-                    if val > max_val:
-                        max_val = val
-                        next_node = node
-            return next_node
-        else:  # Exploration
-            total = 0
-            probabilities = {}
-            for node in unvisited_nodes:
-                if self.graph.exists_edge(current_node, node):
-                    # Meme méthode de calcul de visibilité que pour l'exploitation
-                    visibility = 1.0 / self.graph.get_travel_time(current_node, node)
-
-                    # Pas sur non plus de cette fonction pour le calcul de la probabilité
-                    probabilities[node] = self.pheromones.get((current_node, node), 0) ** self.alpha * visibility ** self.beta
-                    total += probabilities[node]
-            
-            # Aucune phéromone n'a été déposée sur les nœuds voisins
-            if total == 0:
-                return random.choice([n for n in unvisited_nodes if self.graph.exists_edge(current_node, n)]) if unvisited_nodes else None
-            
-            # On choisit un noeud aléatoire en fonction des probabilités calculées
-            r = random.random() * total
-            somme = 0
-            for node, probability in probabilities.items():
-                somme += probability
-                if somme >= r:
-                    return node
-            
-            return next(iter(unvisited_nodes))  # Fallback --> utilisé le chat là dessus mais pas trop compris pk
         
 class Ant_Colony:
-    def __init__(self, id_colony, nb_fourmis, graph):
+    def __init__(self, id_colony, nb_fourmis, graph, noeud_depart, noeud_arrivee, qte_pheromones, alpha=1.0, beta=2.0, rho=0.1, q0=0.9, tau0=None):
         self.id_colony = id_colony
         self.nb_fourmis = nb_fourmis
         self.graph = GlobalGraph()
         self.graph.from_dict(graph)
-        # self.fourmis = [Ant(i, graph.get_random_node(), graph.get_random_node(), self) for i in range(nb_fourmis)]
+        self.fourmis = [Ant(i, noeud_depart, noeud_arrivee, self) for i in range(nb_fourmis)]
+        self.pheromones = {}
+        for i in self.graph.get_nodes().keys():
+            for j in self.graph.get_nodes().keys():
+                if i != j and self.graph.exists_edge(i, j):
+                    self.pheromones[(i, j)] = tau0 if tau0 else 0.1
+        self.alpha = alpha
+        self.beta = beta
+        self.rho = rho
+        self.q0 = q0
+        self.qte_pheromones = qte_pheromones
         
 
 class Ant:
@@ -94,7 +32,7 @@ class Ant:
         self.a_visiter = set()
         self.id = id_fourmi
         self.colonie = colonie
-        self.pheromones = {}
+        self.tps_trajet = 0.0 # Paramètre qui permet de savoir la distance parcourue par la fourmi pour pouvoir distribuer les phéromones de manière homogène par la suite
 
     def voisins(self):
         return self.colonie.graph.get_contingent_nodes(self.noeud_actuel)
@@ -110,7 +48,7 @@ class Ant:
         
         # Choix aléatoire entre exploitation et exploration (basé sur q0)
         q = random.random()
-        if q <= self.q0:  # Exploitation
+        if q <= self.colonie.q0:  # Exploitation
             max_val = -float('inf')
             next_node = None
             for node in self.a_visiter:
@@ -127,37 +65,17 @@ class Ant:
                         next_node = node
             return next_node
         else:  # Exploration
-            total = 0
-            probabilities = {}
-            for node in self.a_visiter:
-                if self.colonie.graph.exists_edge(self.noeud_actuel, node):
-                    # Meme méthode de calcul de visibilité que pour l'exploitation
-                    visibility = 1.0 / self.graph.get_travel_time(self.noeud_actuel, node)[2]
-
-                    # Pas sur non plus de cette fonction pour le calcul de la probabilité
-                    probabilities[node] = self.pheromones.get((self.noeud_actuel, node), 0) ** self.alpha * visibility ** self.beta
-                    total += probabilities[node]
-            
-            # Aucune phéromone n'a été déposée sur les nœuds voisins
-            if total == 0:
-                return random.choice([n for n in self.a_visiter if self.colonie.graph.exists_edge(self.noeud_actuel, n)]) if self.a_visiter else None
-            
-            # On choisit un noeud aléatoire en fonction des probabilités calculées
-            r = random.random() * total
-            somme = 0
-            for node, probability in probabilities.items():
-                somme += probability
-                if somme >= r:
-                    return node
-            
-            return next(iter(self.a_visiter))
-
+            return random.choice([n for n in self.a_visiter if self.colonie.graph.exists_edge(self.noeud_actuel, n)]) if self.a_visiter else None
 
     # Fonction de déplacement de la fourmi
     def deplacement(self):
         while self.a_visiter and not self.objectif:
             # On choisit le prochain noeud à visiter
-            self.noeud_actuel = self.choix_node()
+            # Normalement on est sûr que la fonction choix_node renverra un voisin car si on est dans un cul de sac 
+            # par définition de la condition de sortie du while on n'appellera pas à nouveau la fonction choix_node
+            nouveau_noeud = self.choix_node()
+            self.tps_trajet += self.graph.exists_edge(self.noeud_actuel, nouveau_noeud)[2]
+            self.noeud_actuel = nouveau_noeud
             self.a_visiter = self.voisins()
             self.visites[self.noeud_actuel] = True
             self.objectif = self.noeud_actuel == self.noeud_cible
@@ -166,5 +84,9 @@ class Ant:
         # On ne dépose de phéromone que sur un chemin pour lequel on a atteint l'objectif
         if self.objectif:
             # On dépose la quantité de phéromones attribué pour chaque fourmi de manière proportionnelle à la distance parcourue entre chaque noeud
-            pass
+            ratio_pheromone = self.colonie.qte_pheromones / self.tps_trajet
+            for n1 in self.visites.keys():
+                for n2 in self.visites.keys():
+                    if n1 != n2 and self.colonie.graph.exists_edge(n1, n2):
+                        self.colonie.pheromones[(n1, n2)] += ratio_pheromone * self.graph.exists_edge(n1, n2)[2]
 
